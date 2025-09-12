bb8c6f0f718eeb47eb7fafd0e51ccc70a88717cd
n1ghtRyd3r <joancibersec@gmail.com>
2025-09-12 19:12:39 +0200

    Hola Mundo

--- proyectos/1_Hello_World/hello.c
+++ proyectos/1_Hello_World/hello.c
@@ -1,7 +1,14 @@
+// Incluimos las librerías necesarias para poder ejecutar el archivo 
+
 #include <stdio.h>
+
+// indicamos el inicio de nuestro programa
 int main()
 {
-printf("Hola mundo");
-printf("\n");
-return 0;
-}
+    // Indicamos el contenido del programa y al final indicamos un ";" para indicar 
+    // el final de la línea y "\n" para indicar un salto de línea
+    printf("Hola mundo \n");
+
+    // Indicamos que el programa ha terminado exitosamente
+    return 0;
+};
